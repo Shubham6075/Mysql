3647385e5010d4ce55a02c01002791411c026015
Shubham Rathee <122021456+Shubham6075@users.noreply.github.com>
2023-02-02 14:00:37 +0530

    Add files via upload

---
+++ Creating Database & Tables.sql
@@ -0,0 +1,28 @@
+show databases;                                    -- Showing database 
+
+-- CREATE DATABASE <database_name>;
+
+CREATE DATABASE soap_store;                        -- creating database
+
+
+-- DROP DATABASE <database-name>;                    droping database 
+
+--  USE <database-name>;                              using database
+
+CREATE TABLE cats (
+    name VARCHAR(50),
+    age INT
+);
+ 
+CREATE TABLE dogs (
+    name VARCHAR(50),
+    breed VARCHAR(50),
+    age INT
+);                                                   --   Creating tables 
+
+
+-- DROP TABLE <table-name>;
+
+DROP TABLE cats;                                        -- Droping tables            
+
+    
